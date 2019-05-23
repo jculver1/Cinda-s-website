450fed2d14a26b47b706fa013f8d9ed6b3e0511b
jculver1 <jculver1900@gmail.com>
2019-05-23 09:13:24 -0500

    added home page and navbar

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+    return(
+        <div>
+            <Navbar/>
+            <div>
+                <div class='row header'>
+                    <h1>Cinda's Crafty Creations</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
