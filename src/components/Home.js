9f9c38c977d4e6628095d3f8de2491a863741db3
jculver1 <jculver1900@gmail.com>
2019-05-23 10:24:20 -0500

    added text and columns for about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,18 @@ const Home = () => {
                 <div class='row highlight'>
 
                 </div>
-                <div class='row'>
+                <div class='row mt-5'>
                     <div class='col-6'>
-
+                        <h3>About Us</h3>
+                        <p>
+                        I love cheese, especially cheese on toast queso. Feta swiss boursin feta cheeseburger pecorino port-salut parmesan. Cheesy grin airedale port-salut jarlsberg cream cheese cheeseburger danish fontina cheese on toast. Queso when the cheese comes out everybody's happy pepper jack caerphilly parmesan brie croque monsieur stilton. Cheeseburger cauliflower cheese when the cheese comes out everybody's happy jarlsberg.
+                        </p>
                     </div>
                     <div class='col-6'>
-
+                        <h3>Services</h3>
+                        <p>
+                        I love cheese, especially cheese on toast queso. Feta swiss boursin feta cheeseburger pecorino port-salut parmesan. Cheesy grin airedale port-salut jarlsberg cream cheese cheeseburger danish fontina cheese on toast. Queso when the cheese comes out everybody's happy pepper jack caerphilly parmesan brie croque monsieur stilton. Cheeseburger cauliflower cheese when the cheese comes out everybody's happy jarlsberg.
+                        </p>
                     </div>
                 </div>
             </div>
