643a9ded27556f7d57bebe7dddfc766b292d91a3
jculver1 <jculver1900@gmail.com>
2019-05-26 19:09:34 -0500

    added react admin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Home = () => {
+
     return(
         <div>
             <div class='row highlight'></div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const Services = () => {
     return(
         <div>
-           <h1>Services</h1>
+           <Admin dataProvider={dataProvider} />
         </div>
     )
 }
